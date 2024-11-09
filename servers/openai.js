ddbfdbe08bb308ee527b69e4c534db3a0eaf504c
aymane-omari <aymane.omari.edu@gmail.com>
2024-11-09 14:20:47 -0500

    clean repo

--- servers/openai.js
+++
@@ -1,6 +0,0 @@
-// server/openai.js
-const { Configuration, OpenAIApi } = require('openai');
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-module.exports = new OpenAIApi(configuration);
